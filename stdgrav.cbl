d180acefc8b8b826db42b3457bb49c46e436e1c5
Mica77-Hi <m.motz@free.fr>
2025-02-11 11:27:56 +0100

    Update stdgrav.cbl

--- stdgrav.cbl
+++ stdgrav.cbl
@@ -11,123 +11,123 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
 
-       77 WS-NOME-ALUNO       PIC X(15).
+       77 WS-STUDENT-NAME     PIC X(15).
        77 WS-I                PIC 9(01).
        77 WS-RPT              PIC X(01).
 
-       01 WS-MATERIA.
-          02 WS-NOME-MATERIA  PIC X(10).
+       01 WS-LEARNING.
+          02 WS-LEARNING      PIC X(10).
           02 WS-MEDIA         PIC 9(02)V99.
-          02 WS-RESULTADO     PIC X(10).
-          02 WS-NOTA-1        PIC 9(02).
-          02 WS-NOTA-2        PIC 9(02).
-          02 WS-NOTA-3        PIC 9(02).
-          02 WS-NOTA-4        PIC 9(02).
+          02 WS-RESULT        PIC X(10).
+          02 WS-NOTE-1        PIC 9(02).
+          02 WS-NOTE-2        PIC 9(02).
+          02 WS-NOTE-3        PIC 9(02).
+          02 WS-NOTE-4        PIC 9(02).
 
        PROCEDURE DIVISION.
        P-START.
 
-            INITIALISE WS-MATERIA
-                       WS-NOME-ALUNO
+            INITIALISE WS-LEARNING
+                       WS-STUDENT-NAME
                        WS-I.
 
             DISPLAY "INSERT STUDENT FIRST NAME: "
-            ACCEPT WS-NOME-ALUNO.
+            ACCEPT WS-STUDENT-NAME.
 
             DISPLAY "INSERT GRADE NAME: "
-            ACCEPT WS-NOME-MATERIA OF WS-MATERIA
+            ACCEPT WS-LEARNING OF WS-LEARNING
 
-            PERFORM P-DEFINE-NOTAS    THRU P-DEFINE-NOTAS-FIM
+            PERFORM P-DEFINE-NOTES    THRU P-DEFINE-NOTES-END
                     4 TIMES.
 
-            PERFORM P-CALCULA         THRU P-CALCULA-FIM.
+            PERFORM P-CALCULATE         THRU P-CALCULATE-END.
 
-            PERFORM P-REPETE          THRU P-REPETE-FIM.
+            PERFORM P-REPETE          THRU P-REPETE-END.
 
        P-START-END.
 
-       P-DEFINE-NOTAS.
+       P-DEFINE-NOTES.
             ADD 1 TO WS-I.
 
             DISPLAY "INSERT " WS-I "st GRADE: ".
 
             EVALUATE TRUE
                 WHEN WS-I EQUAL 1
-                     ACCEPT WS-NOTA-1 OF WS-MATERIA
+                     ACCEPT WS-NOTE-1 OF WS-LEARNING
                 WHEN WS-I EQUAL 2
-                     ACCEPT WS-NOTA-2 OF WS-MATERIA
+                     ACCEPT WS-NOTE-2 OF WS-LEARNING
                 WHEN WS-I EQUAL 3
-                     ACCEPT WS-NOTA-3 OF WS-MATERIA
+                     ACCEPT WS-NOTE-3 OF WS-LEARNING
                 WHEN WS-I EQUAL 4
-                     ACCEPT WS-NOTA-4 OF WS-MATERIA
+                     ACCEPT WS-NOTE-4 OF WS-LEARNING
             END-EVALUATE.
 
 
-       P-DEFINE-NOTAS-FIM.
+       P-DEFINE-NOTES-END.
 
-       P-VALIDA-NOTAS.
+       P-PARAGRAPH-1.
 
-            IF WS-NOTA-1 IS <= 0
-                         OR WS-NOTA-1 IS NOT NUMERIC
-                         OR WS-NOTA-1 IS >= 10
+            IF WS-NOTE-1 IS <= 0
+                         OR WS-NOTE-1 IS NOT NUMERIC
+                         OR WS-NOTE-1 IS >= 10
                 DISPLAY '***************************************'
                 DISPLAY '*   GRADE INVALID, PLEASE TRY AGAIN   *'
                 DISPLAY '***************************************'
                 PERFORM P-START THRU P-START-END
             END-IF.
 
-            IF WS-NOTA-2 IS <= 0
-                         OR WS-NOTA-2 IS NOT NUMERIC
-                         OR WS-NOTA-2 IS >= 10
+            IF WS-NOTE-2 IS <= 0
+                         OR WS-NOTE-2 IS NOT NUMERIC
+                         OR WS-NOTE-2 IS >= 10
                 DISPLAY '***************************************'
                 DISPLAY '*   GRADE INVALID, PLEASE TRY AGAIN   *'
                 DISPLAY '***************************************'
                 PERFORM P-START THRU P-START-END
             END-IF.
 
-            IF WS-NOTA-3 IS <= 0
-                         OR WS-NOTA-3 IS NOT NUMERIC
-                         OR WS-NOTA-3 IS >= 10
+            IF WS-NOTE-3 IS <= 0
+                         OR WS-NOTE-3 IS NOT NUMERIC
+                         OR WS-NOTE-3 IS >= 10
                 DISPLAY '***************************************'
                 DISPLAY '*   GRADE INVALID, PLEASE TRY AGAIN   *'
                 DISPLAY '***************************************'
                 PERFORM P-START THRU P-START-END
             END-IF.
 
-            IF WS-NOTA-4 IS <= 0
-                         OR WS-NOTA-4 IS NOT NUMERIC
-                         OR WS-NOTA-4 IS >= 10
+            IF WS-NOTE-4 IS <= 0
+                         OR WS-NOTE-4 IS NOT NUMERIC
+                         OR WS-NOTE-4 IS >= 10
                 DISPLAY '***************************************'
                 DISPLAY '*   GRADE INVALID, PLEASE TRY AGAIN   *'
                 DISPLAY '***************************************'
                 PERFORM P-START THRU P-START-END
             END-IF.
 
-       P-VALIDA-NOTAS-FIM.
+       P-PARAGRAPH-1-END.
 
-       P-CALCULA.
+       P-CALCULATE.
 
-            PERFORM P-VALIDA-NOTAS    THRU P-VALIDA-NOTAS-FIM.
+            PERFORM P-PARAGRAPH-1    THRU P-PARAGRAPH-1-END.
 
-            COMPUTE WS-MEDIA OF WS-MATERIA =
-                   (WS-NOTA-1 OF WS-MATERIA + WS-NOTA-2 OF WS-MATERIA
-                   + WS-NOTA-3 OF WS-MATERIA + WS-NOTA-4 OF WS-MATERIA)
+            COMPUTE WS-MEDIA OF WS-LEARNING =
+                   (WS-NOTE-1 OF WS-LEARNING + WS-NOTE-2 OF WS-LEARNING
+                   + WS-NOTE-3 OF WS-LEARNING + WS-NOTE-4 OF WS-LEARNING)
                    / 4.
 
             IF WS-MEDIA GREATER THAN OR EQUAL 7 THEN
-                MOVE 'APPROVED' TO WS-RESULTADO
+                MOVE 'APPROVED' TO WS-RESULT
             ELSE
-                MOVE 'REPROVED' TO WS-RESULTADO
+                MOVE 'REPROVED' TO WS-RESULT
             END-IF.
 
             DISPLAY '*********** PROCESSING RESULT ***********'
-            DISPLAY 'STUDENT NAME : ' FUNCTION TRIM(WS-NOME-ALUNO)
-            DISPLAY 'GRADE NAME   : ' FUNCTION TRIM(WS-NOME-MATERIA)
+            DISPLAY 'STUDENT NAME : ' FUNCTION TRIM(WS-STUDENT-NAME)
+            DISPLAY 'GRADE NAME   : ' FUNCTION TRIM(WS-LEARNING)
             DISPLAY 'GRADE AVERAGE: ' WS-MEDIA
-            DISPLAY 'RESULT       : ' FUNCTION TRIM(WS-RESULTADO)
+            DISPLAY 'RESULT       : ' FUNCTION TRIM(WS-RESULT)
             DISPLAY '*****************************************'.
 
-       P-CALCULA-FIM.
+       P-CALCULATE-END.
 
        P-REPETE.
             DISPLAY 'REPEAT? (Y/N)'
@@ -137,11 +137,11 @@
                 PERFORM P-START THRU P-START-END.
 
             IF WS-RPT IS EQUAL 'N' OR WS-RPT IS EQUAL 'n'
-                PERFORM P-ENCERRA THRU P-ENCERRA-FIM.
+                PERFORM P-STOP-PGM THRU P-STOP-PGM-END.
 
-       P-REPETE-FIM.
+       P-REPETE-END.
 
-       P-ENCERRA.
+       P-STOP-PGM.
            STOP RUN.
-       P-ENCERRA-FIM.
+       P-STOP-PGM-END.
        END PROGRAM STDGRAV.
